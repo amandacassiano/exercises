60a6810ecd2e6bd8f85a8b34917e3b3b5b73a90c
mandacs <amandacassi4@gmail.com>
2021-11-17 13:09:00 -0300

    upload

---
+++ exercicio2/src/exercicio2/Main.java
@@ -0,0 +1,25 @@
+package exercicio2;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		String name1 = sc.next();
+		int age1 = sc.nextInt();
+		String name2 = sc.next();
+		int age2 = sc.nextInt();
+		
+		double media = (age1 + age2) / 2;
+		
+		System.out.printf("A idade média de %s e %s é de %.1f anos", name1,name2, media);
+		
+		sc.close();
+	}
+
+}
